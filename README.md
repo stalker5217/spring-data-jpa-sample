d10927a8557579688fc49d301a9d527f19ded0f1
Song <stalker5217@gmail.com>
2021-12-04 17:02:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Spring Data JPA 
+
+본 프로젝트는 김영한님의 인프런 강좌 '실전! 스프링 데이터 JPA'의 클론 코딩입니다.
+
+https://inf.run/AGX8
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,25 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
